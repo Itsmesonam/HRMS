a93a190c5d875197884c6eeed2d3c458b6a3e5fc
Itsmesonam <tamusnm999@gmail.com>
2026-08-04 18:02:58 +0545

    admin and user page added and admin css as well

---
+++ admin_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+    <div class="container">
+        
+    <div class="container">
+        <h3>,<span>admin</span></h3>
+        <h1>Welcome <span>Admin</span></h1>
+        <p>this is an admin page</p>
+        <a href="admin_login.php" class="btn">login</a>
+        <a href="admin_register.php" class="btn">register</a>
+        
+    </div>
+</body>
+</html>
---
+++ user_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+    <div class="container">
+        
+    <div class="container">
+        <h3>,<span>user</span></h3>
+        <h1>Welcome <span>User</span></h1>
+        <p>this is a user page</p>
+        <a href="user_login.php" class="btn">login</a>
+        <a href="user_register.php" class="btn">register</a>
+        
+    </div>
+</body>
+</html>
