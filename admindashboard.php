5a0bc9efe2944f8d43bd28aa8af79d8dbf17d32c
Itsmesonam <tamusnm999@gmail.com>
2026-08-26 08:12:24 +0545

    link css

--- admindashboard.php
+++ admindashboard.php
@@ -57,7 +57,7 @@ $totalLandlords = mysqli_fetch_assoc($landlordQuery)['total'];
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
 
     <!-- Admin Dashboard CSS -->
-    <link rel="stylesheet" href="/HRMS/Assets/css/admindashboard_style.css">
+    <link rel="stylesheet" href="assets/css/admindashboard_style.css">
 </head>
 
 <body>
